0f5ab387d50d62897bb4bfbebb12f7b9f991964f
Anton Charov <antoncharov@gmail.com>
2022-01-11 01:09:04 +0300

    change AUTO to IDENTITY in Entity

--- src/main/java/ru/charov/mcone/model/Message.java
+++ src/main/java/ru/charov/mcone/model/Message.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @Setter
 public class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private Integer session_id;
